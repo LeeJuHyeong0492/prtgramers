5651c919bea7345f423215ca7fab6290037764fd
LeeJuHyeong0492 <tkafkr100@naver.com>
2026-02-05 12:42:31 +0900

    [level 1] Title: 예산, Time: 1.26 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        int sum = 0;
+//        int count = 0;
+        for (int n : d){
+            if(budget<n) break;
+            budget -= n;
+            answer++;
+        }
+        return answer;
+    }
+}
