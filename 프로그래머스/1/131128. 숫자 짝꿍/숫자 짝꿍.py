ad2ebf80467d8af73d19d7152aaf5406b55ba355
LeeJuHyeong0492 <tkafkr100@naver.com>
2026-03-12 14:40:10 +0900

    [level 1] Title: 숫자 짝꿍, Time: 93.88 ms, Memory: 26.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,15 @@
+def solution(X, Y):
+    answer = ''
+
+    for i in range(9,-1,-1):
+        char = str(i)
+        count = min(X.count(char),Y.count(char))
+        answer += char*count
+
+    if answer == "":
+        return "-1"
+    
+    if answer[0] == "0":
+        return "0"
+    
+    return answer
